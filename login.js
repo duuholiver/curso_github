b245643f65b8c96f2cd6fa79070da4e77990c8a6
devduuholiver <duuh_oliver@hotmail.com>
2023-08-03 22:46:58 -0300

    Corrigindo o sistema de login da home

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
 //Trabalhando no login
 
 //Finalizando sistemda de login
+
+function login(nome) {
+    return nome;
+}
+
+//Sistema de login completo
+//Retornamos o nome do usuario quando logado
