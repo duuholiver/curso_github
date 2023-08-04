0192598fd9dbc09f21457d977b7be4d64ca0de8f
devduuholiver <duuh_oliver@hotmail.com>
2023-08-03 22:25:39 -0300

    Finalizando secao 3 do curos de git e github

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
 //Trabalhando no login
 
 //Finalizando sistemda de login
+
+function login(nome, email){
+    return 'Bem vindo';
+}
+
+//Sistema de longin completo
+//Retornamos o nome do usuario quando logado
